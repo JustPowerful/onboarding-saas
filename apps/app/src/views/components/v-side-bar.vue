8dbad39f9c17fb245ff32adf76f2b6aa99914d74
JustPowerful <ahmedaminedoudech@gmail.com>
2024-07-27 00:13:06 +0100

    feat: Update sidebar component to use dashboard route instead of support route

--- apps/app/src/views/components/v-side-bar.vue
+++ apps/app/src/views/components/v-side-bar.vue
@@ -17,12 +17,12 @@ const authStore = useAuthStore()
   <section
     id="sidebar"
     :class="[
-      isSideBarOpen ? ' z-50 flex min-w-[256px]' : 'hidden lg:relative lg:z-0 lg:flex lg:w-20'
+      isSideBarOpen ? 'z-50 flex min-w-[256px]' : 'hidden lg:relative lg:z-0 lg:flex lg:w-20'
     ]"
     class="bg-accent h-full min-h-[calc(100vh-5rem)] flex-col justify-between px-6"
   >
     <div class="flex flex-col gap-2 py-4">
-      <router-link v-slot="{ isActive, href, navigate }" custom :to="{ name: 'home-overview' }">
+      <router-link v-slot="{ isActive, href, navigate }" custom :to="{ name: 'dashboard' }">
         <a
           v-bind="$attrs"
           :href="href"
@@ -33,20 +33,6 @@ const authStore = useAuthStore()
           <LayoutDashboard :class="isActive ? 'text-primary' : 'text-muted-foreground'" />
           <h1 :class="[isActive ? 'font-medium' : 'text-muted-foreground']" class="text-sm">
             {{ t('dashboard') }}
-          </h1>
-        </a>
-      </router-link>
-      <router-link v-slot="{ isActive, href, navigate }" custom :to="{ name: 'support-overview' }">
-        <a
-          v-bind="$attrs"
-          :href="href"
-          :class="[isSideBarOpen ? 'flex-row' : 'flex-col text-center']"
-          class="flex items-center gap-2 rounded-lg px-4 py-2"
-          @click="navigate"
-        >
-          <Milestone :class="isActive ? 'text-primary' : 'text-muted-foreground'" />
-          <h1 class="text-sm" :class="[isActive ? 'font-medium' : 'text-muted-foreground']">
-            {{ t('support') }}
           </h1>
         </a>
       </router-link>
