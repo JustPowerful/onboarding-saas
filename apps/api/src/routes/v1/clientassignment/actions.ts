4ecee1c2f7b132c2a82072bb9a512bd6b352e06b
JustPowerful <ahmedaminedoudech@gmail.com>
2024-08-07 01:12:27 +0100

    feat: Add routes for client assignment management

---
+++ apps/api/src/routes/v1/clientassignment/actions.ts
@@ -0,0 +1,113 @@
+import { FastifyPluginAsync } from 'fastify';
+import { ClientAssignmentService } from 'src/services/clientassignment.js';
+import { PipelineService } from 'src/services/pipeline.js';
+
+const routes: FastifyPluginAsync = async (fastify, opt) => {
+  const { prisma } = fastify;
+  const clientAssignmentService = new ClientAssignmentService({ prisma });
+  const pipelineService = new PipelineService({ prisma });
+
+  fastify.post('/create', async (request, reply) => {
+    const { pipeline_id, client_id } = request.body as { pipeline_id: string; client_id: string };
+    const user_id = request.loggedUser.id;
+    try {
+      await clientAssignmentService.createClientAssignment(pipeline_id, client_id, user_id);
+      return reply.send({
+        message: 'Client assignment created successfully',
+      });
+    } catch (error: any) {
+      throw error;
+    }
+  });
+
+  fastify.get('/get/:pipeline_id', async (request, reply) => {
+    const { pipeline_id } = request.params as { pipeline_id: string };
+    const currentUserId = request.loggedUser.id;
+    try {
+      const assignment = await pipelineService.getClientAssignment(pipeline_id, currentUserId);
+      return reply.send({
+        message: 'Client assignment fetched successfully',
+        assignment,
+      });
+    } catch (error) {
+      throw error;
+    }
+  });
+
+  fastify.post('/member/add', async (request, reply) => {
+    const { client_assignment_id, user_id } = request.body as {
+      client_assignment_id: string;
+      user_id: string;
+    };
+    const currentUserId = request.loggedUser.id;
+    try {
+      const member = await clientAssignmentService.addMember(
+        client_assignment_id,
+        user_id,
+        currentUserId,
+      );
+      return reply.send({
+        message: 'The user has been added successfully',
+        member,
+      });
+    } catch (error) {
+      throw error;
+    }
+  });
+  fastify.delete('/member/remove/:client_assignment_id/:user_id', async (request, reply) => {
+    const { client_assignment_id, user_id } = request.params as {
+      client_assignment_id: string;
+      user_id: string;
+    };
+    const currentUserId = request.loggedUser.id;
+    try {
+      await clientAssignmentService.removeMember(client_assignment_id, user_id, currentUserId);
+      return reply.send({
+        message: 'Successfully removed the member from the client assignment',
+      });
+    } catch (error) {
+      throw error;
+    }
+  });
+  fastify.get('/member/getunassigned', async (request, reply) => {
+    const { client_assignment_id, search } = request.query as {
+      client_assignment_id: string;
+      search: string;
+    };
+    try {
+      const members = await clientAssignmentService.getUnassignedMembers(
+        client_assignment_id,
+        search,
+        request.loggedUser.id,
+      );
+      return reply.send({
+        message: 'Successfully fetched unassigned members',
+        members,
+      });
+    } catch (error) {
+      throw error;
+    }
+  });
+  fastify.get('/member/getassigned', async (request, reply) => {
+    const { client_assignment_id, search } = request.query as {
+      client_assignment_id: string;
+      search: string;
+    };
+
+    try {
+      const members = await clientAssignmentService.getAssignedMembers(
+        client_assignment_id,
+        search,
+        request.loggedUser.id,
+      );
+      return reply.send({
+        message: 'Successfully fetched assigned members',
+        members,
+      });
+    } catch (error) {
+      throw error;
+    }
+  });
+};
+
+export default routes;
---
+++ apps/api/src/services/clientassignment.ts
@@ -0,0 +1,215 @@
+import { PrismaClient } from '@saas-monorepo/database';
+import { AbstractServiceOptions } from 'src/types/services.js';
+
+import { PipelineService } from './pipeline.js';
+import { WorkspaceService } from './workspace.js';
+
+export class ClientAssignmentService {
+  prisma: PrismaClient;
+  pipelineService: PipelineService;
+  workspaceService: WorkspaceService;
+  constructor(options: AbstractServiceOptions) {
+    this.prisma = options.prisma;
+    this.pipelineService = new PipelineService(options);
+    this.workspaceService = new WorkspaceService(options);
+  }
+  async getWorkspaceId(client_assignment_id: string) {
+    const client_assignment = await this.prisma.clientAssignment.findUnique({
+      where: {
+        id: client_assignment_id,
+      },
+      select: {
+        pipline: {
+          select: { workspace_id: true },
+        },
+      },
+    });
+    if (!client_assignment) throw new Error('client_assignment_not_found');
+    return client_assignment.pipline.workspace_id;
+  }
+
+  async createClientAssignment(pipeline_id: string, client_id: string, current_user_id: string) {
+    const pipeline = await this.pipelineService.getPipeline(pipeline_id);
+    if (!pipeline) throw new Error('pipeline_not_found');
+    const isMember = await this.workspaceService.isWorkspaceMember(
+      pipeline.workspace_id,
+      current_user_id,
+    );
+    const isOwner = await this.workspaceService.isWorkspaceOwner(
+      pipeline.workspace_id,
+      current_user_id,
+    );
+
+    if (isMember || isOwner) {
+      const latest = await this.prisma.clientAssignment.findFirst({
+        where: {
+          pipeline_id,
+        },
+        select: {
+          pos: true,
+        },
+        orderBy: {
+          pos: 'desc',
+        },
+      });
+      return await this.prisma.clientAssignment.create({
+        data: {
+          pos: latest ? latest.pos + 1 : 0,
+          pipeline_id,
+          client_id,
+        },
+      });
+    } else {
+      throw new Error('unauthorized');
+    }
+  }
+  async removeClientAssignment(client_assignment_id: string, current_user_id: string) {
+    try {
+      const workspace_id = await this.getWorkspaceId(client_assignment_id);
+      const isMember = await this.workspaceService.isWorkspaceMember(workspace_id, current_user_id);
+      const isOwner = await this.workspaceService.isWorkspaceOwner(workspace_id, current_user_id);
+      if (!isMember && !isOwner) throw new Error('unauthorized');
+
+      await this.prisma.clientAssignment.delete({
+        where: {
+          id: client_assignment_id,
+        },
+      });
+    } catch (error) {
+      throw new Error('internal_server_error');
+    }
+  }
+
+  async addMember(client_assignment_id: string, user_id: string, current_user_id: string) {
+    try {
+      const workspace_id = await this.getWorkspaceId(client_assignment_id);
+      const isMember = await this.workspaceService.isWorkspaceMember(workspace_id, current_user_id);
+      const isOwner = await this.workspaceService.isWorkspaceOwner(workspace_id, current_user_id);
+      if (!isMember && !isOwner) throw new Error('unauthorized');
+      const member = await this.prisma.userOnClientAssignment.create({
+        data: {
+          client_assignment_id,
+          user_id,
+        },
+      });
+      return member;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async removeMember(client_assignment_id: string, user_id: string, current_user_id: string) {
+    try {
+      const workspace_id = await this.getWorkspaceId(client_assignment_id);
+      const isMember = await this.workspaceService.isWorkspaceMember(workspace_id, current_user_id);
+      const isOwner = await this.workspaceService.isWorkspaceOwner(workspace_id, current_user_id);
+      if (!isMember || !isOwner) throw new Error('unauthorized');
+      await this.prisma.userOnClientAssignment.deleteMany({
+        where: {
+          user_id: user_id,
+          client_assignment_id: client_assignment_id,
+        },
+      });
+    } catch (error) {
+      throw new Error('internal_server_error');
+    }
+  }
+
+  async getUnassignedMembers(
+    client_assignment_id: string,
+    search: string,
+    current_user_id: string,
+  ) {
+    try {
+      const workspace_id = await this.getWorkspaceId(client_assignment_id);
+      const isMember = await this.workspaceService.isWorkspaceMember(workspace_id, current_user_id);
+      const isOwner = await this.workspaceService.isWorkspaceOwner(workspace_id, current_user_id);
+      if (!isMember && !isOwner) throw new Error('unauthorized');
+      let searchCondition = {};
+      if (search) {
+        searchCondition = {
+          OR: [
+            {
+              firstname: {
+                contains: search,
+                mode: 'insensitive',
+              },
+            },
+            {
+              lastname: {
+                contains: search,
+                mode: 'insensitive',
+              },
+            },
+            {
+              email: {
+                contains: search,
+                mode: 'insensitive',
+              },
+            },
+          ],
+        };
+      }
+      const members = await this.prisma.user.findMany({
+        where: {
+          ...searchCondition,
+          workspaces: {
+            some: {
+              workspace_id: workspace_id,
+            },
+          },
+          user_on_client_assignments: {
+            none: {
+              client_assignment_id: client_assignment_id,
+            },
+          },
+        },
+      });
+      return members;
+    } catch (error) {
+      throw new Error('internal_server_error');
+    }
+  }
+  async getAssignedMembers(client_assignment_id: string, search: string, current_user_id: string) {
+    try {
+      const workspace_id = await this.getWorkspaceId(client_assignment_id);
+      const isOwner = await this.workspaceService.isWorkspaceOwner(workspace_id, current_user_id);
+      const isMember = await this.workspaceService.isWorkspaceMember(workspace_id, current_user_id);
+      if (!isOwner && !isMember) throw new Error('unauthorized');
+      let searchCondition = {};
+      if (search) {
+        searchCondition = {
+          OR: [
+            {
+              firstname: {
+                contains: search,
+                mode: 'insensitive',
+              },
+            },
+            {
+              lastname: {
+                contains: search,
+                mode: 'insensitive',
+              },
+              email: {
+                contains: search,
+                mode: 'insensitive',
+              },
+            },
+          ],
+        };
+      }
+      const members = await this.prisma.user.findMany({
+        where: {
+          ...searchCondition,
+          user_on_client_assignments: {
+            some: {
+              client_assignment_id: client_assignment_id,
+            },
+          },
+        },
+      });
+      return members;
+    } catch (error) {}
+  }
+}
